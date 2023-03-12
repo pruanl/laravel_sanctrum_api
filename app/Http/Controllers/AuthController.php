b30c6429f1d7a007ef32530ae249ac843d65fea6
Pedro Lima <pruan199@gmail.com>
2023-03-12 11:20:59 -0300

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
+            ]);
+
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json($e->getMessage(), 400);
+        }
+
+
+    }
+
+    public function login(Request $request)
+    {
+        try{
+            if (!Auth::attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'message' => 'Invalid login details'
+                ], 401);
+            }
+
+            $user = User::where('email', $request['email'])->firstOrFail();
+
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        }catch (\Exception $e){
+            return response()->json($e->getMessage(), 400);
+        }
+
+    }
+
+    public function me(Request $request)
+    {
+        try{
+            return response()->json(
+                $request->user()
+            );
+        }catch (\Exception $e){
+            return response()->json($e->getMessage(), 400);
+        }
+    }
+}
